3cbec7d4999735f91cc1b2cbcf6a980585862780
Ricardo Pereira <ricardo.pereira@sisloc.com.br>
2023-09-12 15:37:07 -0300

    core.model

--- api/src/plugins/system/core.api.js
+++ api/src/plugins/system/core.api.js
@@ -7,13 +7,6 @@ const router = express.Router()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'PUT, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  next()
-})
-
 const handler = async (req, res) => {  
   const [url] = req.originalUrl.split('?')
   const api = apiConfig[req.method][url]
